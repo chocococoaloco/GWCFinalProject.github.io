e86e587772bced6a8a1bde69a7af3f23bc89257d
chocococoaloco <aschen45@gmail.com>
2019-08-07 10:28:26 -0500

    Website

--- JS/Home.js
+++ JS/Home.js
@@ -1,6 +1,6 @@
 function tbutton() {
   document.getElementById("answer").innerHTML = "";
-  document.getElementById("demo").innerHTML = "Actually, it is not normal because although the earth's climate is always changing, it is changing at an unprecedented rate which cannot be explained by natural cycles of warming and cooling.";
+  document.getElementById("demo").innerHTML = "That is incorrect. It is not normal because although the earth's climate is always changing, it is changing at an unprecedented rate which cannot be explained by natural cycles of warming and cooling.";
 }
 function fbutton() {
   document.getElementById("demo").innerHTML = "";
@@ -8,7 +8,7 @@ function fbutton() {
 }
 function t2button() {
   document.getElementById("buttonf2").innerHTML = ""
-  document.getElementById("buttont2").innerHTML = "That is true, plants absorb CO2 during photosynthesis, but the amount of carbon that is being emitted and produced by humans is so much that there are not enough plants to absorb itâ€¦ especially due to deforestation."
+  document.getElementById("buttont2").innerHTML = "That is incorrect. Plants absorb CO2 during photosynthesis, but the amount of carbon that is being emitted and produced by humans is so much that there are not enough plants to absorb itâ€¦ especially due to deforestation."
 }
 function f2button() {
   document.getElementById("buttont2").innerHTML = ""
@@ -16,7 +16,7 @@ function f2button() {
 }
 function t3button() {
   document.getElementById("buttonf3").innerHTML = ""
-  document.getElementById("buttont3").innerHTML = "Climate change means the increase of the earthâ€™s average temperature of the surface, causing extreme weather to be even more severe such as hurricanes, droughts, heat waves,  and even blizzards."
+  document.getElementById("buttont3").innerHTML = "That is incorrect. Climate change means the increase of the earthâ€™s average temperature of the surface, causing extreme weather to be even more severe such as hurricanes, droughts, heat waves,  and even blizzards."
 }
 function f3button() {
   document.getElementById("buttont3").innerHTML = ""
@@ -24,7 +24,7 @@ function f3button() {
 }
 function t4button() {
   document.getElementById("buttonf4").innerHTML = ""
-  document.getElementById("buttont4").innerHTML = "This is no longer an excuse to not act on climate change, because we are already facing many of the devastating effects on global food supplies, increasing migration, conflict, disease and global instability, and this will only get worse if we donâ€™t act now."
+  document.getElementById("buttont4").innerHTML = "That is incorect. This is no longer an excuse to not act on climate change, because we are already facing many of the devastating effects on global food supplies, increasing migration, conflict, disease and global instability, and this will only get worse if we donâ€™t act now."
 }
 function f4button() {
   document.getElementById("buttont4").innerHTML = ""
@@ -32,7 +32,7 @@ function f4button() {
 }
 function t5button() {
   document.getElementById("buttonf5").innerHTML = ""
-  document.getElementById("buttont5").innerHTML = "Actually, solar polar and onshore wind are the cheapest ways of generating electricity, meaning that the energy they produce is cheaper than using nuclear, gas, and fossil fuels"
+  document.getElementById("buttont5").innerHTML = "That is incorrect. Solar polar and onshore wind are the cheapest ways of generating electricity, meaning that the energy they produce is cheaper than using nuclear, gas, and fossil fuels"
 }
 function f5button() {
   document.getElementById("buttonf5").innerHTML = "You're right."
@@ -40,7 +40,7 @@ function f5button() {
 }
 function t6button() {
   document.getElementById("buttonf6").innerHTML = ""
-  document.getElementById("buttont6").innerHTML = "Because the speed of climate change is so rapid, itâ€™s becoming impossible for animals to adapt to its changing environment."
+  document.getElementById("buttont6").innerHTML = "That is incorrect. Because the speed of climate change is so rapid, itâ€™s becoming impossible for animals to adapt to its changing environment."
 }
 function f6button() {
   document.getElementById("buttont6").innerHTML = ""
@@ -54,4 +54,3 @@ function f7button() {
   document.getElementById("buttont7").innerHTML =""
   document.getElementById("buttonf7").innerHTML = "you right"
 }
-  
